e426df7b962b429a44bf231866b2ceb936514385
mcbobby <madumcbobby@yahoo.com>
2021-05-07 19:51:14 +0100

    add ant design

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import store from './store'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <Provider store={store}>
