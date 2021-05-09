6fde14b59731fc19e6cf0456f297c724e80ac110
HisRoyalWONGiness <talk2wong@gmail.com>
2021-05-09 19:54:42 +0100

    Added the Dashboard Ant Design Feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
 import logo from './logo.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './screens/Home'
 import Dashboard from './screens/Dashboard'
+import { Layout, Menu } from 'antd'
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons'
+import { DashboardStyle } from './DashboardStyle'
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false)
+
+  const { Header, Sider, Content } = Layout;
+
+  const toggle = () => {
+    setCollapsed(!collapsed)
+  }
+
+
   return (
-    <Router>
-      <Route path='/' exact component={Home} />
-      <Route path='/dashboard' component={Dashboard} />
-    </Router>
+    <DashboardStyle>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+              </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+              </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+              </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggle,
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Router>
+              <Route path='/' exact component={Home} />
+              <Route path='/dashboard' component={Dashboard} />
+            </Router>
+          </Content>
+        </Layout>
+      </Layout>
+    </DashboardStyle>
   )
 }
 
